152638b80317cb2af82e5716af7c8c13f97515e3
Admin <admin@Admins-MacBook-Pro.local>
2020-04-13 17:43:29 -0600

    added an ejax call  to post datato my ws

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,6 +1,18 @@
 var submitBotton = $("#submit");
 var form = $("#contactForm");
 
+$.ajax({
+  url: "https://janvier-mbilizi.herokuapp.com/",
+  type: "POST",
+  headers: { Accept: "application/json;" },
+  data: {
+    subject: "subject",
+    message: "some body text",
+  },
+}).done(function (res) {
+  console.log(res); // it shows your email sent message.
+});
+
 submitBotton.on("submit", function () {
   form.reset();
 });
